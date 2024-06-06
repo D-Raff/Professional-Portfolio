09c713e3c53bda88d383bf25c52747e7e0e35331
D-Raff <damonraffels@gmail.com>
2024-06-06 11:53:42 +0200

    strucutre

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="Landing"></div>
+  <div class="About"></div>
+  <div class="Education/Work"></div>
+  <div class="Projects"></div>
+  <div class="Peer Reviews"></div>
+  <div class="Contact"></div>
+</template>
