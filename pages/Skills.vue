cfe95b29282575cd3f89829da21f570d7a92d650
D-Raff <damonraffels@gmail.com>
2024-09-20 10:10:42 +0200

    trying to pull from github

---
+++ pages/Skills.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div v-for="skill in skills" :key="skill.title" class="skills-div">
+            <img :src="skill.logo" alt="skill-logo">
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const {data: backend} = await useFetch("https://d-raff.github.io/VueJS-Portfolio-data/data/");
+    // let skills = backend.skills
+    console.log(backend)
+</script>
+
+<style scoped>
+
+</style>
