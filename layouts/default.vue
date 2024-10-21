59714689dc64886e5b7ac42f0811498e2de0a4dd
Ctec-intern-Jaguar <ctec-capetown-intern@panthera.org>
2024-10-21 17:46:22 +0200

    working on circular nav item. trying to add button that will expand nav and close it when clicked again

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="Nav">
-        <p>Navigation menu</p>
-        <NuxtLink to="/">Home Page</NuxtLink>
-        <NuxtLink to="/skills">Skills and Experience</NuxtLink>
-        <NuxtLink to="/main">main</NuxtLink>
+    <div id="Navigation" class="container">
+        <div id="main-nav" class="container">
+            <button @click="menu()">Navigation menu</button>
+        </div>
+        <NuxtLink to="/" class="nav-item1">Home Page</NuxtLink>
+        <NuxtLink to="/main" class="nav-item2">main</NuxtLink>
+        <NuxtLink to="/skills" class="nav-item3">Skills and Experience</NuxtLink>
     </div>
 
     <div>
-        <NuxtPage :key="$route.fullPath" />
+        <NuxtPage />
     </div>
 </template>
 
 <script setup>
 // Import the skills store
 import { useSkillsStore } from '~/stores/skills';
-import { watchEffect } from 'vue';
+// import { watchEffect } from 'vue';
 
 // Fetch the skills data globally in layout
 const skillsStore = useSkillsStore();
@@ -41,10 +43,63 @@ await skillsStore.fetchSkills();
 //     }
 // });
 
+function menu(){
+    useGsap.to(".nav-item1",{
+        x: -150,
+        y: -100
+    })
+    useGsap.to("#main-nav",{
+        background: "blue"
+    })
+}
+
 </script>
 
 <style scoped>
 .router-link-exact-active {
     color: red;
+}
+
+#Navigation {
+    border: 3px solid red;
+    width: 250px;
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background: black;
+    padding: 0;
+    z-index: 1;
+}
+
+#main-nav{
+    border: 3px blue solid;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    z-index: 1;
+}
+
+:is(.nav-item1, .nav-item2, .nav-item3) {
+    border: 3px red solid;
+    position: absolute;
+    border-radius: 50%;
+    height: 150px !important;
+    width: 150px !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    z-index: 0;
 }
 </style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -1,5 +1,13 @@
 <template>
-    <div id="Skills">
+    <div id="Skills" class="container-fluid">
+        <h1>
+            Tech Stack
+            <section class="stack-under"></section>
+        </h1>
+        <p class="skill-info">
+            With an evergrowing interest in more technologies and languages, I have an ever expanding library of skills
+        <section>I have beginner - intermediate skills with these languages and tools</section>
+        </p>
         <div class="carousel-wrapper container">
             <div class="skill-carousel">
                 <div v-for="skill in skills" :key="skill.title">
@@ -26,7 +34,7 @@ let skills = skillsStore.skills
 
 
 // create a variable for the timeline function of gsap
-// const tl = useGsap.timeline()
+const tl = useGsap.timeline()
 
 function Carousel() {
     let skillCarousel = document.querySelectorAll(".skill-carousel")
@@ -39,11 +47,45 @@ function Carousel() {
     })
 }
 
+function underline() {
+    useGsap.from(".stack-under", {
+        scaleX: 0,
+        stagger: 0.2,
+        duration: 0.5,
+        scrollTrigger: {
+            trigger: ".carousel-wrapper",
+            start: "top 40%",
+            end: "top 21%",
+            toggleActions: "restart reverse restart reverse",
+            markers: {
+                startColor: "purple",
+                endColor: "red",
+                fontSize: "20px"
+            }
+        }
+    })
+    useGsap.from(".skill-info", { scaleY: '0', stagger: .2, duration: .7, ease: "back", delay: .5,
+    scrollTrigger: {
+            trigger: ".carousel-wrapper",
+            start: "top 40%",
+            end: "top 21%",
+            toggleActions: "restart reverse restart reverse",
+            markers: {
+                startColor: "purple",
+                endColor: "red",
+                fontSize: "20px"
+            }
+        }
+    })
+}
+
 onMounted(() => {
+    underline()
     useGsap.from(".skill-img", {
         y: 200,
         stagger: 0.2,
         duration: 0.5,
+        delay: .5,
         ease: "back",
         scrollTrigger: {
             trigger: ".carousel-wrapper",
@@ -65,6 +107,7 @@ onMounted(() => {
 
 <style scoped>
 #Skills {
+    font-family: "Share Tech Mono", monospace;
     min-height: 200vh;
     display: flex;
     flex-direction: column;
@@ -73,12 +116,17 @@ onMounted(() => {
     align-items: center;
 }
 
+.skill-info {
+    font-family: Electrolize;
+    color: #67c7eb;
+    text-align: center;
+}
+
 .carousel-wrapper {
     display: flex;
     overflow: hidden;
     width: fit-content;
     height: fit-content;
-    border: 3px solid red;
 }
 
 .skill-carousel {
@@ -88,8 +136,23 @@ onMounted(() => {
 }
 
 .skill-img {
-    width: 150px;
+    height: 150px;
+    aspect-ratio: 1/1;
     margin: 20px;
-    object-fit: cover;
+    object-fit: contain;
+    box-shadow: inset #67c7eb 0 0 10px 1px;
+    border-radius: 10px;
+    padding: 10px;
+}
+
+h1 {
+    font-size: 100px;
+    color: #aa0505;
+}
+
+.stack-under {
+    border: #67c7eb 1px solid;
+    width: 100%;
+    box-shadow: #67c7eb 0px 0px 5px 2px;
 }
 </style>
